47d192f0d335f0306a3a9363f5367428aedafd18
Сергей <seregabur@live.ru>
2022-03-27 19:44:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Цыклы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите колличество повторов сообщения");
+            int numberIterations =  Convert.ToInt32( Console.ReadLine());
+
+           for(int i=0;i<numberIterations;i++)
+            {
+            Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
